e84c84d00f5f36c9183db5ebec04f04bf54d3108
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-12-08 10:36:57 +0530

    adding login  :zap:

--- servertest.js
+++ servertest.js
@@ -2,6 +2,7 @@ const express = require('express')
 const fs = require('fs')
 const cp = require('child_process')
 const path = require('path')
+const { error } = require('console')
 const app = express()
 app.use(express.json())
 function createUser(userName){
@@ -15,4 +16,14 @@ else{
     console.log('user already exists please login')
 }
 }
-createUser('Manoja')
+function login(userName) {
+    const Dirpath = path.join(__dirname,`gitcat/${userName}`)
+    if(fs.existsSync(Dirpath)){
+        fs.readdir(Dirpath,(err,files) => {
+   if(err) throw err
+   console.log('files in directory : ',files)
+        })
+    }
+}
+// createUser('Manoja')
+login("Aditya")
